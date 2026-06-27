b2ae0ea1b384e4229bd338d68789121f27d50b25
Matheus <matheusmoraesdj2025@gmail.com>
2026-06-27 19:33:20 +0000

    Landing: single hero section inspired by MindMarket in hsl(97,57%,61%)

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,62 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
 import { Header } from '../../components/header';
 import { Footer } from '../../components/footer';
-import { PropertiesService } from '../../services/properties';
-import { HlmButton } from '@spartan-ng/helm/button';
-import { HlmBadge } from '@spartan-ng/helm/badge';
-import { HlmSeparator } from '@spartan-ng/helm/separator';
-import { HlmAspectRatio } from '@spartan-ng/helm/aspect-ratio';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import {
-  lucideHome,
-  lucideShield,
-  lucideMapPin,
-  lucideBedDouble,
-  lucideBath,
-  lucideCar,
-  lucidePhone,
-  lucideArrowRight,
-  lucideSearch,
-  lucideBuilding,
-} from '@ng-icons/lucide';
-import { HlmH1, HlmH2, HlmH3, HlmLead } from '@spartan-ng/helm/typography';
 
 @Component({
   selector: 'app-landing',
-  imports: [
-    RouterLink,
-    Header,
-    Footer,
-    HlmButton,
-    HlmBadge,
-    HlmSeparator,
-    HlmAspectRatio,
-    NgIcon,
-    HlmH1,
-    HlmH2,
-    HlmH3,
-    HlmLead,
-  ],
-  providers: [
-    provideIcons({
-      lucideHome,
-      lucideShield,
-      lucideMapPin,
-      lucideBedDouble,
-      lucideBath,
-      lucideCar,
-      lucidePhone,
-      lucideArrowRight,
-      lucideSearch,
-      lucideBuilding,
-    }),
-  ],
+  imports: [Header, Footer],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
-export class Landing {
-  protected readonly propertiesService = inject(PropertiesService);
-  protected readonly featuredSale = this.propertiesService.forSale.slice(0, 3);
-  protected readonly featuredRent = this.propertiesService.forRent.slice(0, 3);
-}
+export class Landing {}
