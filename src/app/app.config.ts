36fd9598ae47529e911abfa4bf4136a5a86ffca9
Matheus <matheusmoraesdj2025@gmail.com>
2026-06-27 16:08:39 +0000

    fix: add explicit CDK overlay/dialog providers for Angular 22 service registration + fix Leaflet import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -28,7 +28,12 @@ import {
 } from '@ng-icons/lucide';
 import { provideSpartanHlm } from '@spartan-ng/helm/utils';
 import { DialogModule } from '@angular/cdk/dialog';
-import { OverlayModule } from '@angular/cdk/overlay';
+import { OverlayModule, Overlay, OverlayContainer, OverlayPositionBuilder, OverlayOutsideClickDispatcher, ScrollStrategyOptions } from '@angular/cdk/overlay';
+import { A11yModule } from '@angular/cdk/a11y';
+import { PortalModule } from '@angular/cdk/portal';
+import { CdkMenuModule } from '@angular/cdk/menu';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { Directionality } from '@angular/cdk/bidi';
 
 import { routes } from './app.routes';
 
@@ -38,7 +43,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideAnimations(),
-    importProvidersFrom(DialogModule, OverlayModule),
+    importProvidersFrom(DialogModule, OverlayModule, A11yModule, PortalModule, CdkMenuModule, ScrollingModule),
+    { provide: Overlay, useClass: Overlay },
+    { provide: OverlayContainer, useClass: OverlayContainer },
+    { provide: OverlayPositionBuilder, useClass: OverlayPositionBuilder },
+    { provide: OverlayOutsideClickDispatcher, useClass: OverlayOutsideClickDispatcher },
+    { provide: ScrollStrategyOptions, useClass: ScrollStrategyOptions },
+    { provide: Directionality, useClass: Directionality },
     provideSpartanHlm(),
     provideIcons({
       lucideSun,
