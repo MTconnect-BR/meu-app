8329d64f495a7a29a29fa0ead585c07d2ee862a0
Matheus <matheusmoraesdj2025@gmail.com>
2026-06-29 01:57:26 +0000

    feat: redesign menu with MindMarket visual style - green burger, large typography, beige borders, yellow CTA

--- src/app/components/header.ts
+++ src/app/components/header.ts
@@ -1,15 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideMenu, lucideHome, lucideX } from '@ng-icons/lucide';
+
 import { AuthService } from '../services/auth';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, NgIcon],
-  providers: [
-    provideIcons({ lucideMenu, lucideHome, lucideX }),
-  ],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
